e786a5d01b9c2b99ec9f269cf94f85c9ac24aec5
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-10-12 16:02:41 -0300

    feat: create person repository class

---
+++ src/main/java/com/crudapi/crud/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.crudapi.crud.repositories;
+
+import com.crudapi.crud.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, String> {
+}
