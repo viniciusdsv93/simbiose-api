abc2d6d206a1a0889c0b40a009d75f9ccea3f719
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-10-12 16:01:15 -0300

    feat: create person entity class

---
+++ src/main/java/com/crudapi/crud/entities/Person.java
@@ -0,0 +1,94 @@
+package com.crudapi.crud.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pessoa")
+public class Person implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  private String id;
+
+  @Column(name = "nome", length = 100, nullable = false)
+  private String name;
+
+  @Column(name = "email", length = 150, nullable = false)
+  private String email;
+
+  @Column(name = "data_nascimento", columnDefinition = "DATE")
+  private LocalDate birthday;
+
+  public Person() {
+
+  }
+
+  public Person(String id, String name, String email, LocalDate birthday) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.birthday = birthday;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public LocalDate getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(LocalDate birthday) {
+    this.birthday = birthday;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Person person = (Person) o;
+    return Objects.equals(id, person.id) && Objects.equals(email, person.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, email);
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            ", email='" + email + '\'' +
+            ", birthday=" + birthday +
+            '}';
+  }
+}
