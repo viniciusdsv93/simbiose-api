0b02f32838069efa5289e85b591efa94ad5e9501
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-10-14 08:01:11 -0300

    fix: remove table name annotation

--- src/main/java/com/crudapi/crud/entities/Person.java
+++ src/main/java/com/crudapi/crud/entities/Person.java
@@ -3,13 +3,11 @@ package com.crudapi.crud.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
-@Table(name = "pessoa")
 public class Person implements Serializable {
 
   private static final long serialVersionUID = 1L;
